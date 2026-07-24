9085ccc7460da519749c68d7c28a3621d923bead
manadapatitas <manadapatitasveterinaria@gmail.com>
2026-07-24 16:48:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // =================================================================
 
 // ⚠️ REEMPLAZA ESTA URL CON LA URL DE TU WEB APP DE GOOGLE APPS SCRIPT (debe terminar en /exec)
-const URL_BACKEND = "https://script.google.com/macros/s/TU_ID_DE_DESPLIEGUE_AQUI/exec";
+const URL_BACKEND = "https://script.google.com/macros/s/AKfycby5LdWif3Eum4dAAyuqBHUON3C17OW4SLbeRxoutLyYneHcFGfQ_Q4OqwoGBCRESrcF/exec";
 
 // Estado global de la aplicación
 let datosGlobales = {
